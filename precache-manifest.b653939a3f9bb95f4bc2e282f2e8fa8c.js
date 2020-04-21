016355cfe9c36509e985846607fa6d6621ace5d4
Suhaib Khan <suheb.work@gmail.com>
2020-04-21 11:49:33 +0100

    Updates

---
+++ precache-manifest.b653939a3f9bb95f4bc2e282f2e8fa8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2af5d256eb76b09a0ec4daacb215a9b",
+    "url": "/mars-rover/index.html"
+  },
+  {
+    "revision": "0396da5653b0ba7e4698",
+    "url": "/mars-rover/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "073ab2173d006aa78652",
+    "url": "/mars-rover/static/js/2.88ff30a4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mars-rover/static/js/2.88ff30a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0396da5653b0ba7e4698",
+    "url": "/mars-rover/static/js/main.e9829519.chunk.js"
+  },
+  {
+    "revision": "a7fd7dd75512b14e3362",
+    "url": "/mars-rover/static/js/runtime-main.0b1953fb.js"
+  }
+]);
